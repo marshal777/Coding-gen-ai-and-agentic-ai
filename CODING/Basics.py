8776aacf20f0e3f00320f6ad054b60d7d419d4e1
Marshal_0 <103542482+marshal777@users.noreply.github.com>
2025-04-22 17:33:07 +0530

    Add files via upload

---
+++ CODING/Basics.py
@@ -0,0 +1,33 @@
+# Data Types
+# Variables can store data of different types, and different types can do different things.
+
+# Python has the following data types built-in by default, in these categories:
+
+# Text Type: str(string)
+# Numeric Types: int(integer), float(floating value )
+
+
+
+
+
+
+
+
+vasi = 21
+
+
+print(vasi)
+
+
+
+
+
+
+
+# complex(complex value )
+# Sequence Types: list, tuple, range
+# Mapping Type: dict
+# Set Types: set, frozenset
+# Boolean Type: bool
+# Binary Types: bytes, bytearray, memory view
+# None Type: NoneType
